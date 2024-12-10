2fea51e2a08056d2b63bf1d2cd997f779bdf4d42
KaRinaTrick <proxorova.karina36@gmail.com>
2024-12-10 18:53:23 +0300

    Add files via upload

---
+++ call counter.py
@@ -0,0 +1,31 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+    
+
+
+def string_info(string):
+    print((len(string)), string.upper(), string.lower())
+    count_calls()
+
+
+def is_contains(string, list_to_search):
+    list_to_search = [i.upper() for i in list_to_search]
+    string = string.upper()
+    if string in list_to_search:
+        print(True)
+    else:
+        print(False)
+    print(string, list_to_search)
+    count_calls()
+
+
+string_info('Capybara')
+string_info('Armageddon')
+string_info('Karina')
+is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])
+is_contains('cycle', ['recycling', 'cyclic'])
+print(calls)
