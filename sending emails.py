1ea9c6e3f7eddc35652e3c4b7886dfc3015bc183
KaRinaTrick <proxorova.karina36@gmail.com>
2024-12-10 21:00:58 +0300

    Add files via upload

---
+++ sending emails.py
@@ -0,0 +1,19 @@
+def send_email (message, recipient, *, sender = "university.help@gmail.com"):
+    end_file = (".com",".ru",".net")
+    if_end = sender.endswith(end_file, -4) == recipient.endswith(end_file, -4) #пометка для себя - не обязательно все писать в условии, можно создать отдельные переменные!!!!!!!!!!!!!
+    if if_end is not True:
+        if recipient.count('@') == 1 and sender.count('@') == 1:
+            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+
+
+# send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+# send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+# send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+# send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
